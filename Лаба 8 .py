115f36a0ce015bfd644d1fe96f48c942892b9815
NikitaDolotov1 <122666407+NikitaDolotov1@users.noreply.github.com>
2023-06-17 07:34:17 +0300

    Update Лаба 8 .py

--- Лаба 8 .py
+++ Лаба 8 .py
@@ -1,84 +1,57 @@
-class H1(BaseException):
+class Exception1(BaseException):
     pass
 
-
-class H2(BaseException):
+class Exception2(BaseException):
     pass
 
-
-class H3(BaseException):
+class Exception3(BaseException):
     pass
 
+class DataTypes:
+    def str(self, data):
+        if not isinstance(data, str):
+            raise Exception1("Необходимо написать текст")
+        return print(data)
 
-class Ashan_Class:
-    def method1(self, argument):
-        if argument < 0:
-            raise H1("Argument should be non-negative")
-        elif argument == 0:
-            raise H2("Argument should be non-zero")
-        else:
-            return argument ** 2
+    def int(self, data):
+        if not isinstance(data,int):
+            raise Exception2("Необходимо написать число")
+        return print(data)
 
-    def method2(self, argument1, argument2):
-        if argument1 == argument2:
-            raise H3("Arguments should be different")
-        else:
-            return argument1 + argument2
-
-    def method3(self, argument):
-        if not isinstance(argument, str):
-            raise TypeError("Argument should be a string")
-        elif len(argument) < 5:
-            raise ValueError("String should be at least 5 characters long")
-        else:
-            return argument.upper()
+    def list(self, data):
+        if not isinstance(data, list):
+            raise Exception3("Необходимо написать список")
+        return print(data)
 
 
-class Processing:
-    def __init__(self, obj):
-        self.obj = obj
+class FixErorr:
+    def __init__(self):
+        self.fix = DataTypes()
 
-    def fix_method1(self, argument):
+    def fixStr(self, data):
         try:
-            result = self.obj.method1(argument)
-        except H1 as x:
-            print("Error:", x)
-            result = None
-        except H2 as x:
-            print("Error:", x)
-            result = None
-        return result
+            self.fix.str(data)
 
-    def fix_method2(self, argument1, argument2):
+
+        except Exception1:
+            print("Введено", data, "Необходимо написать текст")
+
+    def fixInt(self, data):
         try:
-            result = self.obj.method2(argument1, argument2)
-        except H3 as x:
-            print("Error:", x)
-            result = None
-        return result
+            self.fix.int(data)
 
-    def fix_method3(self, argument):
+        except Exception2:
+            print("Введено", data, "Необходимо написать число")
+
+    def fixList(self, data):
         try:
-            result = self.obj.method3(argument)
-        except TypeError as x:
-            print("Error:", x)
-            result = None
-        except ValueError as x:
-            print("Error:", x)
-            result = None
-        return result
+            self.fix.list(data)
+            print(data)
+        except Exception3:
+            print("Введено",data, " Необходимо написать список")
 
+f = FixErorr()
 
-obj = Ashan_Class()
-proc = Processing(obj)
-
-print(proc.proc_method1(2))
-print(proc.proc_method1(-1))
-
-print(proc.proc_method2(2, 3))
-print(proc.proc_method2(2, 2))
-
-print(proc.proc_method3("hello"))
-print(proc.proc_method3(123))
-
-print(proc.proc_method3("hi"))
+f.fixStr(1334)
+f.fixInt("Привет")
+f.fixList(2)
